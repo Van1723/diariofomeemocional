b9d794e4244b3d6fd523876d545ede5567fc9db1
Van1723 <vanessa.tecma@gmail.com>
2026-02-05 16:53:00 -0300

    layout_diaryForm

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,46 @@ import Home from "./frontEnd/pages/Home";
 import DiaryForm from "./frontEnd/pages/DiaryForm";
 import ListaRegistro from "./frontEnd/pages/ListaRegistro";
 import RegistroItem from "./frontEnd/pages/RegistroItem";
+import FormRegistro from "./frontEnd/pages/FormRegistro";
+import Navbar from "./frontEnd/components/Navbar";
 import { useState } from "react";
 
 function App() {
- const [registros, setRegistros] = useState([]);
- 
+  const [registros, setRegistros] = useState([]);
+
   function adicionarRegistro(novoRegistro) {
-    setRegistros(prev => [...prev, novoRegistro]);
+    setRegistros((prev) => [...prev, novoRegistro]);
   }
 
   return (
-    <>
+    <div className="app"style={{display:"flex", flexDirection:"column"}} >
+
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+
         <Route
           path="/diary"
           element={<DiaryForm onSubmit={adicionarRegistro} />}
         />
+
         <Route
           path="/lista"
           element={<ListaRegistro registros={registros} />}
-
         />
+
         <Route
           path="/registro/:id"
           element={<RegistroItem registros={registros} />}
         />
-        <Route path="/registro" element={<p>Selecione um registro</p>} />
 
-
-
+        <Route path="/formRegistro" element={<FormRegistro />} />
       </Routes>
-    </>
+
+    </div>
   );
 }
-  
 
 export default App;
